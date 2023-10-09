f3ee1357b3d69577602e7f6b7498b85610b2e731
Cishy <fedorovnikita04@yandex.ru>
2023-10-09 16:01:08 +0300

    feat: добавлены исключения для работы с сущностью режиссера

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/DirectorSaveException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class DirectorSaveException extends RuntimeException {
+
+    public DirectorSaveException(String message) {
+        super(message);
+    }
+}
