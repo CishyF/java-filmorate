1e59e6618062af9b228bf05568b201f6fa1f7935
Cishy <fedorovnikita04@yandex.ru>
2023-10-09 15:59:34 +0300

    feat: добавлены сущность режиссера и список режиссеров у фильма

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+public class Director {
+
+    private int id;
+    @EqualsAndHashCode.Exclude
+    @NotBlank(message = "Имя не должно быть пустым")
+    private String name;
+}
