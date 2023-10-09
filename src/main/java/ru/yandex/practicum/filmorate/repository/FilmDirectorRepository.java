179bcf8d52bf3f19e6e560a26bc46be6fc3c30ac
Cishy <fedorovnikita04@yandex.ru>
2023-10-09 16:01:28 +0300

    feat: добавлены репозитории для работы с таблицами director и film_director

---
+++ src/main/java/ru/yandex/practicum/filmorate/repository/FilmDirectorRepository.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.repository;
+
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
+public interface FilmDirectorRepository {
+
+    void saveDirectors(Film film);
+
+    void loadDirectors(List<Film> films);
+
+    void deleteDirectors(Film film);
+
+    void deleteFilmsOfDirectorById(int id);
+}
